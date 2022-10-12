3222f1d841bc6734dd56bd3822d450365ba4e8e5
Anuj Mukesh Gupta <anuj.gupta@quantiphi.com>
2022-10-12 15:57:02 +0530

    leaderboards

--- src/components/quiz/QuizCard.js
+++ src/components/quiz/QuizCard.js
@@ -1,6 +1,6 @@
 // import * as React from 'react';
 import { useTheme } from "@mui/material/styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -9,18 +9,14 @@ import "./QuizCard.css";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useNavigate } from "react-router-dom";
-
-import {db} from "../../firebase-config"
 import {
-  getDocs,
-  collection,
-  deleteDoc,
+  getDoc,
   doc,
-  query,
-  where,
+  deleteDoc,
 } from "firebase/firestore";
+import { db } from "../../firebase-config"
 
-export default function QuizCard({ isadmin,name,tag,id,user,score,q,getql}) {
+export default function QuizCard({ isadmin, name, tag, id, user, score, q, getql }) {
   let navigate = useNavigate();
   const theme = useTheme();
   const [show, setShow] = useState(false);
@@ -37,7 +33,40 @@ export default function QuizCard({ isadmin,name,tag,id,user,score,q,getql}) {
     await deleteDoc(postDoc);
     getql();
   };
-  
+  // For Leaderboard
+  const [winners, setWinners] = useState([])
+  const docRef = doc(db, "quizz", id);
+
+  useEffect(() => {
+    const getSingleQuizz = async () => {
+      const docSnap = await getDoc(docRef);
+
+      var usersdata = docSnap.data().users;
+      console.log()
+
+      var userlist = Object.keys(usersdata).map(
+        (key) => {
+          return ({
+            "name": key,
+            "score": usersdata[key]["score"]
+          })
+        }
+      )
+      userlist.sort(function (a, b) {
+        return b.score - a.score
+      })
+
+      setWinners(userlist.slice(0, 3))
+    };
+    getSingleQuizz();
+  }, []);
+  // const getQuizz = async () => {
+  //   const data = await getDocs(quizCollectionRef);
+  //   setQuizList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   // console.log(quizLists);
+  // };
+  // getQuizz();
+
   return (
     <Card className="card" sx={{ display: "flex", alignItems: "center" }}>
       <div
@@ -63,14 +92,24 @@ export default function QuizCard({ isadmin,name,tag,id,user,score,q,getql}) {
             <a onClick={handleShow} className="leaderboard">
               View Leaderboard
             </a>
+
             <Modal show={show} onHide={handleClose}>
               <Modal.Header closeButton>
                 <Modal.Title>Top 3</Modal.Title>
               </Modal.Header>
               <Modal.Body>
-                1:Ravi<br/>
-                2:Jj<br/>
-                3:Hh<br/>
+                {
+                  winners.map(
+                    (winner) => {
+                      return (
+                        <div>
+                          <span>{winner.name.split("@")[0].trim()} : {winner.score} points</span>
+                          <br />
+                        </div>
+                      )
+                    }
+                  )
+                }
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
@@ -78,16 +117,18 @@ export default function QuizCard({ isadmin,name,tag,id,user,score,q,getql}) {
                 </Button>
               </Modal.Footer>
             </Modal>
+
+
             <Modal show={show1} onHide={handleClose1}>
               <Modal.Header closeButton>
                 <Modal.Title>Quiz Information</Modal.Title>
               </Modal.Header>
               <Modal.Body>
-                10 minutes time <br/>
+                10 minutes time <br />
                 please complete it on time!
               </Modal.Body>
               <Modal.Footer>
-                <Button variant="primary" onClick={()=>navigate(`/quiz/${id}`)}>
+                <Button variant="primary" onClick={() => navigate(`/quiz/${id}`)}>
                   Start
                 </Button>
               </Modal.Footer>
@@ -95,32 +136,33 @@ export default function QuizCard({ isadmin,name,tag,id,user,score,q,getql}) {
           </CardContent>
         </Box>
         {isadmin ? (
-          <div style={{width:'20%',display:'flex',justifyContent:'space-around',alignItems:'center',
-  
-          
+          <div style={{
+            width: '20%', display: 'flex', justifyContent: 'space-around', alignItems: 'center',
+
+
           }}>
             <button className="btn btn-secondary">Update</button>
             <button className="btn btn-danger"
-            onClick={() => {
-              deletePost(id)
-            }}>Delete</button>
+              onClick={() => {
+                deletePost(id)
+              }}>Delete</button>
           </div>
         ) : (
           <>
-          {!user.completed?
-          <img
-            onClick={() => setShow1(true)}
-            className="arrow"
-            src="https://img.icons8.com/material-outlined/48/000000/circled-right.png"
-          />
-          :
-          <div style={{display:'flex',justifyContent:'space-between',alignItems:'space-between',width:'40%'}}>
-          <div style={{fontSize:'40px',fontFamily:'fantasy'}}>
-          {score}/{q.length}
-          </div>
-          <img style={{marginRight:'10px'}} src="https://img.icons8.com/material-two-tone/48/000000/checked--v1.png"/>
-          </div>
-          }
+            {!user.completed ?
+              <img
+                onClick={() => setShow1(true)}
+                className="arrow"
+                src="https://img.icons8.com/material-outlined/48/000000/circled-right.png"
+              />
+              :
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'space-between', width: '40%' }}>
+                <div style={{ fontSize: '40px', fontFamily: 'fantasy' }}>
+                  {score}/{q.length}
+                </div>
+                <img style={{ marginRight: '10px' }} src="https://img.icons8.com/material-two-tone/48/000000/checked--v1.png" />
+              </div>
+            }
           </>
         )}
       </div>
